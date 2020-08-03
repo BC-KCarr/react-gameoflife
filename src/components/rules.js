47a679c24ba8e7f5a808cb2cd6af20612f8898df
Kevin <kevincarr632@gmail.com>
2020-08-02 19:37:41 -0500

    cleaned up styling

--- src/components/rules.js
+++ src/components/rules.js
@@ -3,18 +3,20 @@ import React from 'react'
 function Rules() {
   return (
     <div style={{ width: '30%', textAlign: 'center'}}>
-      <h3>Rules</h3>
+      <h3 style={{ marginBottom: '25px'}}>Rules</h3>
       <h4>For a space that is 'populated'</h4>
+      <div style={{ textAlign: 'left'}}>
       <p>
-        Each cell with one or no neighbors dies, as if by solitude.
-        Each cell with four or more neighbors dies, as if by overpopulation.
-        Each cell with two or three neighbors survives. 
+        Each cell with one or no neighbors dies, as if by solitude.<br/>
+        Each cell with four or more neighbors dies, as if by overpopulation.<br/>
+        Each cell with two or three neighbors survives.<br/> 
       </p>
+      </div>
       <h4>For a space that is 'empty' or 'unpopulated'</h4>
       <p>
         Each cell with three neighbors becomes populated.
       </p>
-      <h3>The Controls</h3>
+      <h3 style={{ marginTop: '85px'}}>The Controls</h3>
       <p>
         Choose a figure from either the oscillators or spaceships pull-down menu or make one yourself by clicking on the cells with a mouse. A new generation of cells (corresponding to one iteration of the Rules) is initiated by the 'Start' button. The 'Start' button advances the game by several generations. Game grid-size is regulated by the grid size pull-down.
       </p>
